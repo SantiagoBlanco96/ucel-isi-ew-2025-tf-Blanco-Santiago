a0f0c59381ad1d463f4084d386285bdecddfca4b
SantiagoBlanco96 <sam96.sb73@gmail.com>
2026-03-12 12:24:30 -0300

    add initial HTML structure and .gitignore for project files

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generador de Noticias con IA</title>
+</head>
+<body>
+
+    <footer>
+        <p>UCEL - ISI - Entornos Web - Trabajo final: Blanco, Santiago</p>
+    </footer>
+
+</body>
+</html>
+
